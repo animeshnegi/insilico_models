b7553cc897aef08227f05d5a72fe84c4d2406e85
animeshnegi <73010551+animeshnegi@users.noreply.github.com>
2021-06-08 13:19:51 +0530

    Add files via upload

---
+++ 1st run me before me.py
@@ -0,0 +1,16 @@
+
+
+# you have to add more option in model page 
+
+
+# just copy all stuff which is printed using command Print(x)  and then paste it in  110 line after "var choices =  paste here" 
+
+# copy with brackkets also ['']
+
+import os
+x = next(os.walk('downloads/models'))[2]
+print(x) 
+
+
+
+
---
+++ README.md
@@ -0,0 +1,11 @@
+
+This is the source code of in-silico models
+
+all the things you need to change like id or folder destination like uploads
+and downloadable folder path are present in config.json 
+
+Change the path as related to your computer server
+
+
+
+
