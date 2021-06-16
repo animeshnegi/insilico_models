44bfe189a39a6896cc88819242a13099414f0a63
animeshnegi <73010551+animeshnegi@users.noreply.github.com>
2021-06-16 23:30:51 +0530

    Update 1st run me before me.py

--- 1st run me before me.py
+++ 1st run me before me.py
@@ -1,11 +1,11 @@
 
 
-# you have to add more option in model page 
+# To add more suggestions in search model page 
 
 
-# just copy all stuff which is printed using command Print(x)  and then paste it in  110 line after "var choices =  paste here" 
+# Copy all stuff which is printed using command Print(x), and then paste it in search page 110 line after var choices = "paste here" 
 
-# copy with brackkets also ['']
+# copy [''] also 
 
 import os
 x = next(os.walk('downloads/models'))[2]
